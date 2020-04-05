677aa2b15ee3888ef93550246f477eb94c05034b
slukas111 <sashamati@gmail.com>
2020-04-05 13:17:59 -0700

    created people and craft function

--- iss.py
+++ iss.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
-__author__ = 'Sasha Lukas'
+__author__ = 'Sasha Lukas + demo'
 
 
-def main():
-    pass
+def main(args):
+    #Part A: Name of astronauts and crafts
+    nauts_dict = get_astronauts()
+    print('\nCurrent people in Space: {}'.format(len(nauts_dict)))
+    for naut in nauts_dict:
+        print(' - {}'.fomat(naut['name'], naut['craft']))
+
 
 
 if __name__ == '__main__':
